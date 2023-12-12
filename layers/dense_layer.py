7c7979b5dda6f7a7cfec767976a69f295435a6f6
IlyaKuz26 <ilyaprime26@yandex.ru>
2023-12-12 12:46:03 +0700

    Add dense_layer.py

---
+++ layers/dense_layer.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Dense_Layer:
+  # Layer initialization
+  def __init__(self, n_inputs, n_neurons):
+    self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+    self.biases = 0.1 * np.ones((1, n_neurons))
+
+  # Forward pass
+  def forward(self, inputs):
+    self.inputs = inputs
+    self.output = np.dot(inputs, self.weights) + self.biases
+
+  # Backward pass
+  def backward(self, dvalues):
+    # Gradients of parametrs
+    self.dweights = np.dot(self.inputs.T, dvalues)
+    self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+    # Gradients of inputs
+    self.dinputs = np.dot(dvalues, self.weights.T)
